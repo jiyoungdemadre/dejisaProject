9990490ad159623de088e117ba477511140a7a04
jiyoungdemadre <55229437+jiyoungdemadre@users.noreply.github.com>
2019-11-27 16:33:28 +0100

    classes

---
+++ Classes/Artist.php
@@ -0,0 +1,9 @@
+<?php
+
+class Artist{
+    private $artist_id;
+    private $name;
+    private $bio;
+    private $gender;
+    private $date_of_birth;
+}
--- Classes/Song.php
+++ Classes/Song.php
@@ -7,4 +7,106 @@ class Song{
     private $categ_id;
     private $artist_id;
 
+    
+
+
+    /**
+     * Get the value of song_id
+     */ 
+    public function getSong_id()
+    {
+        return $this->song_id;
+    }
+
+    /**
+     * Set the value of song_id
+     *
+     * @return  self
+     */ 
+    public function setSong_id($song_id)
+    {
+        $this->song_id = $song_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of release_date
+     */ 
+    public function getRelease_date()
+    {
+        return $this->release_date;
+    }
+
+    /**
+     * Set the value of release_date
+     *
+     * @return  self
+     */ 
+    public function setRelease_date($release_date)
+    {
+        $this->release_date = $release_date;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of categ_id
+     */ 
+    public function getCateg_id()
+    {
+        return $this->categ_id;
+    }
+
+    /**
+     * Set the value of categ_id
+     *
+     * @return  self
+     */ 
+    public function setCateg_id($categ_id)
+    {
+        $this->categ_id = $categ_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of artist_id
+     */ 
+    public function getArtist_id()
+    {
+        return $this->artist_id;
+    }
+
+    /**
+     * Set the value of artist_id
+     *
+     * @return  self
+     */ 
+    public function setArtist_id($artist_id)
+    {
+        $this->artist_id = $artist_id;
+
+        return $this;
+    }
 }
